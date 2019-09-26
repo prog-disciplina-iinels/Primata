0c757f3e9949639307e03020226b00e1e0411773
Erika <erikagarcia512@gmail.com>
2019-09-26 10:24:46 -0300

    codigo primata com laços

--- primata.py
+++ primata.py
@@ -1,3 +1,4 @@
+from random import randint
 habituacao = bool(input("O animal está habituado: "))
 
 som = 0
@@ -7,77 +8,44 @@ agua = 0
 tentativa = 0
 
 tempo = 0
-tentativa2 = 0
+tentativa1 = 0
 
-if(habituado):
-   aproximacao = 30
-   
-   aproximacao = int(input("O animal se aproximou quanto? "))
-   tentativa = int(input("Qual o numero da tentativa atual? "))
-   
-   if(aproximacao < 30 and tentaviva <= 20):
-      agua = 0.5
-   agua = 0.0
-      
-   if(tentativa > 20): 
-      print("O primata esta apto para a proxima fase")
+while(not habituado):
+       habituado = bool(2)
 
-   aproximacao = 30   
-   aproximacao = int(input("O animal se aproximou quanto? "))
-   tentativa = int(input("Qual o numero da tentativa atual? "))
+print("O animal se encontra habituado")
+aprox = 30
+dist1 = 0
+
+for i in range(20):
+       
+   aproximacao = randint(0, 31)
    
-   if(aproximacao < 30 and tentaviva <= 20)
+   if aproximacao > 30 or aproximacao < 0:
+      continue
+
+    aprox = aprox - dist1
+if aprox == 0:
       agua = 0.5
-   agua = 0.0
-   if(tentativa > 20): 
-      print("O primata esta apto para a proxima fase")
-            
-   aproximacao = 30   
-   aproximacao = int(input("O animal se aproximou quanto? "))
-   tentativa = int(input("Qual o numero da tentativa atual? "))
-   
-   if(aproximacao < 30 and tentaviva <= 20):
-      agua = 0.5
-   agua = 0.0
-   
-   if(tentativa >= 20): 
-      print("O primata esta apto para a proxima fase")   
-   
-      print("Iniciando 2a fase")
-      
-      agua = 0.0
-      som = int(input("Qual o som escutado, 1 ou 2?"))
-      lado = int(input("Qual lado escolhido pelo primata, 1-esquerda ou 2-direita?"))
-      
-      if((som == 1 and lado == 1) or (som == 2 and lado == 2)):
-         agua = 0.5
-         som = 0
-         lado = 0
-      
-      agua = 0.0
-      som = int(input("Qual o som escutado, 1 ou 2?"))
-      lado = int(input("Qual lado escolhido pelo primata, 1-esquerda ou 2-direita?"))
-      
-      if((som == 1 and lado == 1) or (som == 2 and lado == 2)):
-         agua = 0.5
-         som = 0
-         lado = 0
-      agua = 0.0
-      som = int(input("Qual o som escutado, 1 ou 2?"))
-      lado = int(input("Qual lado escolhido pelo primata, 1-esquerda ou 2-direita?"))
-      
-      if((som == 1 and lado == 1) or (som == 2 and lado == 2)):
-         agua = 0.5
-         som = 0
-         lado = 0
-      tempo = int(input("Qual foi a duração do experimento, em minutos?"))
-      tentiva2 = int(input("Quantas tentativas foram executadas?"))
-      
-      if(tempo >= 30 and tentativa2 >= 50):
-         print("O primata esta apto para a proxima fase")  
-      else:
-         print("O primata nao esta apto para a proxima fase")  
-   else:
-      print("O primata nao esta apto para a proxima fase")  
+      print("O animal tocou a barra")
+      break
+print("O animal pasa para a proxima fase")
+
+agua = 0
+tentativa = 0
+
+for i in range(50):
+
+   som = randint(2)
+   lado = randint(2)
+   if(som and lado):
+    agua += 0.5
+   if (not som and not lado)):
+    agua += 0.5   
+
+    print("O animal recebeu "%agua" ml de água")
+minut = int(input("Tempo da última etapa (minutos)? "))
+if(minut >= 30):
+       print("O animal  realizou todas as fases")
 else: 
-   print("O animal nao esta habituado")
+   print("O animal não realizou a última fase no tempo hábil")
